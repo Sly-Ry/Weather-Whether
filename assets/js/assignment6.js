63944b69021a096b8516915f68ba1b423a816bac
ryan <ryan.williams93@aol.com>
2021-12-15 19:16:05 -0600

    bootstrap input

--- assets/js/assignment6.js
+++ assets/js/assignment6.js
@@ -1,6 +1,8 @@
+var apiKey = ["2632ab542fff737012a28d74931b6af5"];
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#search-npt")
-var wContainerEl = document.querySelector("#weather-container");
+var wContainerEl = document.querySelector(".w-container");
+
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -15,17 +17,16 @@ var formSubmitHandler = function(event) {
     else {
         alert("Please enter the name of a city.");
     }
-    
-    console.log(event);
 };
 
 // gets the lat and lon coordinates in order to find city
 var getLatLon = function(cityName) {
-    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=2632ab542fff737012a28d74931b6af5"
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + apiKey;
 
     fetch(apiUrl)
     .then(function(response) {                     
         response.json().then(function(data) {
+           getCityWeather(data[0].lat, data[0].lon);
            console.log(data);
         });
     })
@@ -35,46 +36,52 @@ var getLatLon = function(cityName) {
     
 };
 
-
-
-
-
-
 // gets weather for search city
-var getCityWeather = function() {
-    var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+var getCityWeather = function(lat, lon) {
+    var wApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&units=imperial&appid=" + apiKey;
+    
+    fetch(wApiUrl)
+    .then(function(response) {                     
+        response.json().then(function(data) {
+            displayWeather(data);
+            console.log(data.current);
+            console.log(data.daily);
+        });
+    })
+    .catch (function(error){
+        alert("Unable to connect to source")
+    });
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
 
-// // create a var that gets user repos
-// var getUserRepos = function(user){
-
-//     // format the github api url
-//     var apiUrl = "https://api.github.com/users/"+ user +"/repos";
+var displayWeather = function(data) {
     
-//     // fetch from app and a response came from GitHub's server.
-//     // make a request to the url
-//     fetch(apiUrl)
-//     .then(function(response) {
+    console.log(data.current);
+    
+    // var nameEl  = document.createElement("h3")
+    // nameEl.classList = "city-name"
+    // nameEl.textContent = cityName;
+    // wContainerEl.append(nameEl);
 
-//         // an if statement in case of 404 status on username search
-//         if (response.ok) {
+    // create variables using weather data
+    var tempEl = document.createElement("p");
+    tempEl.textContent = "Temp: " + data.current.temp + "";
+    wContainerEl.append(tempEl);
 
-//             // json is used here, but sometimes "text() would be used if resources return non-json data"
-//             response.json().then(function(data) {
-//                 displayRepos(data, user);
-//             });
-//         }
-//         else {
-//             alert("Github User Not Found!");
-//         }
-//         // console.log("inside", response);
-//     })
-//     .catch(function(error) {
-        
-//         // notice this '.catch()' getting chained onto the end of the '.then()' method
-//         alert("Unable to connect to Github");
-//     });
-//     // console.log("outside");
-// };
+    var humidityEl = document.createElement("p");
+    humidityEl.textContent = "Humidity: " + data.current.humidity + "%";
+    wContainerEl.append(humidityEl);
+
+    var wSpeedEl = document.createElement("p");
+    wSpeedEl.textContent = "Wind: " + data.current.wind_speed + " MPH";
+    wContainerEl.append(wSpeedEl);
+
+    var uviEl = document.createElement("p");
+    uviEl.classList = "success";
+    uviEl.textContent = "UVI Index: " + data.current.uvi;
+    wContainerEl.append(uviEl);
+
+};
+
+formSubmitHandler();
