1dd6aa7db74b8b35925190de994a559b2805d691
ryan <ryan.williams93@aol.com>
2022-10-02 16:53:13 -0500

    set background change based on city name

--- index.js
+++ index.js
@@ -18,13 +18,21 @@ let weather = {
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
 
-        document.querySelector(".city").innerText = "Weather in " + name
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png"
+        document.querySelector(".city").innerText = 
+            "Weather in " + name
+        document.querySelector(".icon").src = 
+            "https://openweathermap.org/img/wn/" + icon + ".png"
         document.querySelector(".description").innerText = description
-        document.querySelector(".temp").innerText = temp + "°C"
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity +"%"
-        document.querySelector(".wind").innerText = "W Speed: " + speed + " km/h"
-        document.querySelector(".weather").classList.remove("loading") 
+        document.querySelector(".temp").innerText = 
+            temp + "°C"
+        document.querySelector(".humidity").innerText = 
+            "Humidity: " + humidity +"%"
+        document.querySelector(".wind").innerText = 
+            "W Speed: " + speed + " km/h"
+        
+        document.querySelector(".weather").classList.remove("loading");
+
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
     },
     search: function() {
         this.getWeather(document.querySelector(".search-bar").value);
